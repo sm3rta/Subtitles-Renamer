4215afc2233cba8a82ac948c9e8e32ce8d89c878
Ahmed <sm3rta@yahoo.com>
2018-05-17 02:51:45 +0200

    Update Subtitles Renamer.py
    Cleaner
    Supports all possible video and subtitle extensions
    Takes a backup into a "subtitles backup" folder

--- Subtitles Renamer.py
+++ Subtitles Renamer.py
@@ -1,23 +1,79 @@
 import os
-print("This tool is developed by SMARTA!")
+from time import sleep
+from shutil import rmtree, copy2
+#print("This tool is developed by Smarta")  #A bit cheesy
+
+sub_exts=['aqt',
+         '.ass',
+         '.gsub',
+         '.jss',
+         '.pjs',
+         '.psb',
+         '.rt',
+         '.sbv',
+         '.smi',
+         '.srt',
+         '.srt',
+         '.ssa',
+         '.ssf',
+         '.stl',
+         '.sub',
+         '.ttxt',
+         '.usf']
+epi_exts=['3g2',
+         '.3gp',
+         '.abi',
+         '.amv',
+         '.asf',
+         '.drc',
+         '.f4a',
+         '.f4b',
+         '.f4p',
+         '.f4v',
+         '.flv',
+         '.gif',
+         '.gifv',
+         '.m2v',
+         '.m4p',
+         '.m4v',
+         '.mkv',
+         '.mng',
+         '.mov',
+         '.mp2',
+         '.mp4',
+         '.mpe',
+         '.mpeg',
+         '.mpg',
+         '.mpv',
+         '.mxf',
+         '.mxf',
+         '.nsv',
+         '.ogg',
+         '.ogv',
+         '.qt',
+         '.rm',
+         '.rmvb',
+         '.roq',
+         '.roq',
+         '.svi',
+         '.vob',
+         '.webm',
+         '.wmv',
+         '.yuv']
+
+
 while True:
     folder=input("Please enter the full path to the folder containing both videos and subtitles\n")
-    a=os.listdir(folder)
-
-    ext1=a[0][-3:]
-    ext2=a[1][-3:]
-    sub_exts=["srt","ass","sub"]
-    epi_exts=["mp4","mkv","avi","wmv","flv","m4v", "mpg"]
+    files=os.listdir(folder)
 
     subs, epis = [], []
 
-    for item in a:
-        ext=item[-3:]
+    for file in files:
+        _, ext = os.path.splitext(file)
         if ext in sub_exts:
-            subs.append(item)
+            subs.append(file)
         elif ext in epi_exts:
-            epis.append(item)
-    #print(subs, epis)        
+            epis.append(file)
 
     if len(subs)==0 and len(epis)==0:
         print("There are no videos or subtitles!")
@@ -26,14 +82,23 @@ while True:
     elif len(epis)==0:
         print("There are no videos!")
     elif len(subs)!=len(epis):
-        print("The number of videos isn't equal to that of subtitles. Quitting.")
+        print("The number of videos isn't equal to that of subtitles.")
     else:
+        #taking a backup first
+        if "subtitles backup" in os.listdir(folder):
+            rmtree(os.path.join(folder, "subtitles backup"))
+            sleep(1)
+        os.makedirs(os.path.join(folder, "subtitles backup"))
+        for sub in subs:
+            copy2(os.path.join(folder, sub), os.path.join(folder, "subtitles backup"))
+            
+        #rename subs
         index=0
         while index<len(epis):
-            sub_ext=subs[index][-3:]
-            print(subs[index], "->", epis[index][:-3]+sub_ext)
+            _, sub_ext = os.path.splitext(subs[index])
+            epi_name, _ = os.path.splitext(epis[index])
+            print(subs[index], "->", epi_name+sub_ext)
             
-            os.rename(folder+"\\"+subs[index],folder+"\\"+epis[index][:-3]+sub_ext)
+            os.rename(folder+"\\"+subs[index],folder+"\\"+epi_name+sub_ext)
             index+=1
-
     input("Press Enter to give me another job!")
